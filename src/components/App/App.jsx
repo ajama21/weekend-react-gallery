a318ff7133e191e34b18b8287278cfc9d63da249
Abdirahman Jama <mustalid21@gmail.com>
2023-09-14 16:48:06 -0500

    installed required packages and added one image

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,7 +8,8 @@ function App() {
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <img src="images/goat_small.jpg"style={{ width: 'auto', height: '150px', marginRight: '20px'}}/>
+        <img src="images/my_family.jpg"style={{ width: 'auto', height: '500px', borderRadius: '50%'}} />
       </div>
     );
 }

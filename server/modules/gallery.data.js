0960bde959ad09af61d55863c922bec38cabb579
Abdirahman Jama <mustalid21@gmail.com>
2023-09-16 16:09:23 -0500

    added data

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,7 +1,9 @@
 const galleryItems = [
     { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
     { id: 2, path: 'images/Somali_flag.jpg', description: 'Photo of the somali flag', likes: 0},
-    { id: 3, path: 'Kaba.webp', description: 'Photo of the Holy Kaba in Saudi Arabia', likes: 0}
+    { id: 3, path: 'images/Kaba.webp', description: 'Photo of the Holy Kaba in Saudi Arabia', likes: 0},
+    {id: 4, path: 'images/javascript.jpg', description:'Photo of JavaScript logo', likes: 0},
+    {id: 5, path: 'images/onepiece.avif', description: 'Photo of One Piece anime characters', likes: 0}
 ];
 
 module.exports = galleryItems;

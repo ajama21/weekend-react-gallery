18733179b7b7fd36f55c1c5821c0dff4067bbfbe
Abdirahman Jama <mustalid21@gmail.com>
2023-09-16 16:10:01 -0500

    added css styling

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,32 +7,7 @@ import Header from "../Header/Header";
 
 function App() {
 
-  let [galleryItems, setGalleryItem] = useState([]);
 
-  // Call function so it runs once on component load
-  // Similar to jQuery's document ready
-  useEffect(() => {
-    fetchGalleryList();
-  }, []);
-
-        // Function to get the item from the server/database
-        const fetchGalleryList = () => {
-          axios({
-            method: 'GET',
-            url: '/gallery'
-          })
-            .then( (response) => {
-              console.log('Entire response:', response);
-              // The actual array comes from the data attribute on the response
-              // console.log('Just the data:', response.data);
-      
-              // Set data into component state
-              setGalleryItem(response.data);
-            })
-            .catch(function (error) {
-              console.log('Error on get:', error);
-            });
-        }
 
   return (
     <div className="App">

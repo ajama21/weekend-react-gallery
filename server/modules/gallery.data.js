c4fa43b0572f63ef76de4bd3e9a38c4de8e4c242
Abdirahman Jama <mustalid21@gmail.com>
2023-09-16 22:32:50 -0500

    added image

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,9 +1,10 @@
 const galleryItems = [
     { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 2, path: 'images/Somali_flag.jpg', description: 'Photo of the somali flag', likes: 0},
+    { id: 2, path: 'images/Somali_flag.jpg', description: 'Photo of the Somali flag', likes: 0},
     { id: 3, path: 'images/Kaba.webp', description: 'Photo of the Holy Kaba in Saudi Arabia', likes: 0},
     {id: 4, path: 'images/javascript.jpg', description:'Photo of JavaScript logo', likes: 0},
-    {id: 5, path: 'images/onepiece.avif', description: 'Photo of One Piece anime characters', likes: 0}
+    {id: 5, path: 'images/onepiece.avif', description: 'Photo of One Piece anime characters', likes: 0},
+    {id: 6, path: 'images/basketball.jpg', description: 'Photo of a basketball on the edge of a rim without a net', likes: 0}
 ];
 
 module.exports = galleryItems;
